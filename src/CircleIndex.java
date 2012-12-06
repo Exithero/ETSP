a4f2fe3a8a08dd4e098a055b4d0c55c38e414e39
mbernt <mbernt.mbernt@gmail.com>
2012-12-06 18:39:59 +0100

    Small change in description in CircleIndex.

--- src/CircleIndex.java
+++ src/CircleIndex.java
@@ -5,7 +5,7 @@
  * example use
  * int[] arr={1,2,3,4,5}
  * CircleIndex ci=new CircleIndex(arr.length)
- * ci.set(5);
+ * ci.set(3);
  * for(int i=0;i<arr.length;i++){
  * 	System.out.println(arr[ci.get()]);
  * 	ci.increment();
